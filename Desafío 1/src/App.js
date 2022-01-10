befc7698381004766995e628c677136e1a434965
gmussocoder <musso.guillermo.capacitacion@gmail.com>
2022-01-10 11:16:54 -0300

    10012022

---
+++ Desafío 1/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DESAFÍO N° 1 <br></br>
+          Alumno: Guillermo Andrés Musso Rodríguez <br></br>
+          e-Mail: gamussorodriguez@teco.com.ar <br></br>
+          Móvil: 1130743868 <br></br>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
