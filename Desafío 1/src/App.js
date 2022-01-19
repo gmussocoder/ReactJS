7df06ddd2c16bef930441fbdd6544210ed6880f2
gmussocoder <musso.guillermo.capacitacion@gmail.com>
2022-01-19 02:13:54 -0300

    19012022

--- Desafío 1/src/App.js
+++ Desafío 1/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import inicio from './Pages/Inicio';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Router>
+          <Navbar/>
+      </Router>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,11 +22,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          DESAFÍO N° 1 <br></br>
+          DESAFÍO N° 2 <br></br>
           Alumno: Guillermo Andrés Musso Rodríguez <br></br>
           e-Mail: gamussorodriguez@teco.com.ar <br></br>
           Móvil: 1130743868 <br></br>
         </a>
+        <button type="button" className="btn btn-primary">Primary</button>
+        <button type="button" className="btn btn-secondary">Secondary</button>
+        <button type="button" className="btn btn-success">Success</button>
       </header>
     </div>
   );
