d674a251813af45e95a35ddc7f2acc55a56ea52d
gmussocoder <musso.guillermo.capacitacion@gmail.com>
2022-01-23 15:15:56 -0300

    21012022

--- Desafío 1/src/components/CartWidget.js
+++ Desafío 1/src/components/CartWidget.js
@@ -1,47 +1,39 @@
 import React from "react";
-import { useReducer, useState } from "react";
-
-const initialState = { contador: 0 };
-
-const TYPES = {
-  INCREMENT:"INCREMET",
-  INCREMENT5:"INCREMENT5",
-  DECREMENT:"DECREMENT",
-  DECREMENT5:"DECREMENT5",
-  RESET:"RESET"
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case TYPES.INCREMENT:
-      return { contador: state.contador + 1 };
-    case TYPES.DECREMENT:
-      return { contador: state.contador - 1 };
-    default:
-      return state;
-  }
-}
-
-const Contador = () => {
-  //const [contador, setContador] = useState(0);
-  const [state, dispacth] = useReducer(reducer, initialState);
-  //const sumar = () => setContador(contador + 1);
-  const sumar = () => dispacth({ type:TYPES.INCREMENT });
-  //const restar = () => setContador(contador - 1);
-  const restar = () => dispacth({ type:TYPES.DECREMENT });
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+  
+function Contador() {
+  const [itemCount, setItemCount] = React.useState(4);
   return (
-    <div style={{ textAlign:"center" }}>
-      <h2>Contador Reducer</h2>
-      <nav>
-        <button>+5</button>
-        <button onClick={sumar}>+</button>
-        <button>0</button>
-        <button onClick={restar}>-</button>
-        <button>-5</button>
-      </nav>
-      <h3>{state.contador}</h3>
+    <div style={{ display: "block", padding: 30 }}>
+      <div>
+        <Badge color="secondary" badgeContent={itemCount}>
+          <ShoppingCartIcon />{" "}
+        </Badge>
+        <ButtonGroup>
+          <Button
+            onClick={() => {
+              setItemCount(Math.max(itemCount - 1, 0));
+            }}
+          >
+            {" "}
+            <RemoveIcon fontSize="small" />
+          </Button>
+          <Button
+            onClick={() => {
+              setItemCount(itemCount + 1);
+            }}
+          >
+            {" "}
+            <AddIcon fontSize="small" />
+          </Button>
+        </ButtonGroup>
+      </div>
     </div>
   );
-};
-
+}
 export default Contador;
